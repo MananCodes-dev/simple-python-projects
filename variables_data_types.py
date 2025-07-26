0372b9ea5006f30b74ffa2d7e2b7f13080c458e6
Manan Kumar <manank3326@gmail.com>
2025-07-26 10:52:02 +0530

    Add files via upload

---
+++ variables_data_types.py
@@ -0,0 +1,8 @@
+# This is the code for string operation just a example 
+# where we make a new string from two strings
+var1 = "It's a Sunday"
+var2 = "HAve a great day!"
+
+var3 = var1[:5] + var2[5:13] + var1[6:]
+
+print(var3)
